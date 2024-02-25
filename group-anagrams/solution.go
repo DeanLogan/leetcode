d9a72787c3b0538d554c1180a848ab093a755e57
Dean Logan <deanlogan42@gmail.com>
2024-02-25 22:00:09 +0000

    Completed group anagrams

---
+++ group-anagrams/solution.go
@@ -0,0 +1,39 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+	fmt.Println(groupAnagrams([""]))
+	fmt.Println(groupAnagrams(["a"]))
+}
+
+import (
+	"sort"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	anagramGroups := make(map[string][]string)
+
+	for _, s := range strs {
+		sorted := sortString(s)
+		anagramGroups[sorted] = append(anagramGroups[sorted], s)
+	}
+
+	groupedAnagrams := [][]string{}
+	for _, group := range anagramGroups {
+		groupedAnagrams = append(groupedAnagrams, group)
+	}
+
+	return groupedAnagrams
+}
+
+func sortString(s string) string {
+	bytes := []byte(s)
+	sort.Slice(bytes, func(i, j int) bool {
+		return bytes[i] < bytes[j]
+	})
+	return string(bytes)
+}
