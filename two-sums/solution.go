855e59439c2fcb91e03a8c96fe3cc78b81e76f42
DeanLogan <deanlogan42@gmail.com>
2024-03-08 08:49:45 +0000

    Completed two sum problem

---
+++ two-sums/solution.go
@@ -0,0 +1,23 @@
+package main;
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(twoSum([2,7,11,15], 9))
+	fmt.Println(twoSum([3,2,4], 6))
+	fmt.Println(twoSum([3,3], 6))
+}
+
+func twoSum(nums []int, target int) []int {
+    numMap := make(map[int]int)
+    for i, num := range nums {
+        containsValue := target - num
+        if index, exists := numMap[containsValue]; exists {
+            return []int{index, i}
+        }
+        numMap[num] = i
+    }
+    return []int{}
+}
