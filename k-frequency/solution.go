1a2a3da54ea6f8f5d2f7a6cf53ec6b013b0e6370
Dean Logan <deanlogan42@gmail.com>
2024-03-14 18:47:09 +0000

    Completed k frequency problem

---
+++ k-frequency/solution.go
@@ -0,0 +1,33 @@
+import (
+	"fmt"
+	"sort"
+)
+
+func main(){
+	fmt.Println(topKFrequent([]int{1,1,1,2,2,3}))
+	fmt.Println(topKFrequent([]int{}))
+}
+
+func topKFrequent(nums []int, k int) []int {
+    frequency := make(map[int]int)
+    for _, num := range nums {
+        frequency[num]++
+    }
+
+    mapToSlice := [][]int{}
+    for key, value := range frequency {
+        mapToSlice = append(mapToSlice, []int{key,value})
+    }
+
+    sort.Slice(mapToSlice, func(i, j int) bool {
+        return mapToSlice[i][1] > mapToSlice[j][1]
+    })
+
+    mostFrequentNums := make([]int, k)
+
+    for i, _ := range mostFrequentNums {
+        mostFrequentNums[i] = mapToSlice[i][0]
+    }
+
+    return mostFrequentNums
+}
