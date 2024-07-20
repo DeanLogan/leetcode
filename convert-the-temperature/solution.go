9aaaccaab980b7d9b357c54639119894f1efb918
Dean Logan <deanlogan42@gmail.com>
2024-07-20 21:22:53 +0100

    Completed convert the temperature problem

---
+++ convert-the-temperature/solution.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println(convertTemperature(36.50))
+	fmt.Println(convertTemperature(122.11))
+}
+
+func convertTemperature(celsius float64) []float64 {
+    return []float64{celsius + 273.15, (celsius * 1.8) + 32.00}
+}
