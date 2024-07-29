ff4d2dc7648473d83e19a1bf889d4febf93d2193
Dean Logan <deanlogan42@gmail.com>
2024-07-29 16:55:31 +0100

    Completed merge two sorted lists problem

---
+++ merge-two-sorted-lists/solution.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	l1 := &ListNode{Val: 1}
+	l1.Next = &ListNode{Val: 2}
+	l1.Next.Next = &ListNode{Val: 4}
+	l2 := &ListNode{Val: 1}
+	l2.Next = &ListNode{Val: 3}
+	l2.Next.Next = &ListNode{Val: 4}
+	fmt.Println(mergeTwoLists(l1, l2))
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+
+	if l1 != nil {
+		current.Next = l1
+	} else {
+		current.Next = l2
+	}
+
+	return dummy.Next
+}
