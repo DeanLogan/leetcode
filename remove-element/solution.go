551db9a5f612f893cb7ac5262663fc48c07fe804
Dean Logan <deanlogan42@gmail.com>
2024-08-31 18:11:32 +0100

    Completed remove element problem

---
+++ remove-element/solution.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
+	fmt.Println(removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2))
+}
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	return i
+}
