23ca17c46c0cc7de2c61dfd691c48cfcce897c02
Dean Logan <deanlogan42@gmail.com>
2024-09-06 21:18:27 +0100

    Completed merge sorted array problem

---
+++ merge-sorted-array/solution.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    fmt.Println(merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3))
+    fmt.Println(merge([]int{1}, 1, []int{}, 0))
+    fmt.Println(merge([]int{0}, 0, []int{1}, 1))
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) []int {
+    p1 := m-1
+    p2 := n-1
+    p := m+n-1
+
+    for p1 >= 0 && p2 >= 0 {
+        if nums1[p1] > nums2[p2] {
+            nums1[p] = nums1[p1]
+            p1--
+        } else {
+            nums1[p] = nums2[p2]
+            p2--
+        }
+        p--
+    }
+
+    for p2 >= 0 {
+        nums1[p] = nums2[p2]
+        p--
+        p2--
+    }
+
+	return nums1
+}
