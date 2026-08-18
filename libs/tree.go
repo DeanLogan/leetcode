ed674cbd2865eca54f6d8f88a4fe2fd8db39f91c
Dean Logan <deanlogan42@gmail.com>
2026-08-18 14:15:54 +0100

    added function that will print trees in a more visual manner

--- libs/tree.go
+++ libs/tree.go
@@ -47,6 +47,99 @@ func BuildTree(values []int) *TreeNode {
 	return root
 }
 
+// PrintTreeFancy prints trees like this:
+//	        4
+//	       / \
+//	      2   7
+//	     / \ / \
+//	    1  3 6  9
+func PrintTreeFancy(root *TreeNode) {
+	if root == nil {
+		fmt.Println("<empty>")
+		return
+	}
+
+	lines := buildFancyLines(root)
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
+
+func buildFancyLines(node *TreeNode) []string {
+	if node == nil {
+		return []string{}
+	}
+
+	valStr := strconv.Itoa(node.Val)
+
+	if node.Left == nil && node.Right == nil {
+		return []string{valStr}
+	}
+
+	if node.Left == nil {
+		rightLines := buildFancyLines(node.Right)
+		rightWidth := len(rightLines[0])
+		firstLine := valStr + strings.Repeat(" ", rightWidth)
+		secondLine := strings.Repeat(" ", len(valStr)) + "\\" + strings.Repeat(" ", rightWidth-1)
+		result := []string{firstLine, secondLine}
+		padding := strings.Repeat(" ", len(valStr))
+		for _, line := range rightLines {
+			result = append(result, padding+line)
+		}
+		return result
+	}
+
+	if node.Right == nil {
+		leftLines := buildFancyLines(node.Left)
+		leftWidth := len(leftLines[0])
+		firstLine := strings.Repeat(" ", leftWidth) + valStr
+		secondLine := strings.Repeat(" ", leftWidth-1) + "/" + strings.Repeat(" ", len(valStr))
+		result := []string{firstLine, secondLine}
+		padding := strings.Repeat(" ", 0)
+		for _, line := range leftLines {
+			result = append(result, padding+line)
+		}
+		return result
+	}
+
+	leftLines := buildFancyLines(node.Left)
+	rightLines := buildFancyLines(node.Right)
+
+	leftWidth := len(leftLines[0])
+	rightWidth := len(rightLines[0])
+
+	// Build first line: spaces + root value centered above
+	firstLine := strings.Repeat(" ", leftWidth) + valStr + strings.Repeat(" ", rightWidth)
+
+	// Build connector line with / and \
+	secondLine := strings.Repeat(" ", leftWidth-1) + "/" + strings.Repeat(" ", len(valStr)) + "\\" + strings.Repeat(" ", rightWidth-1)
+
+	result := []string{firstLine, secondLine}
+
+	// Merge left and right subtree lines side by side
+	leftLen := len(leftLines)
+	rightLen := len(rightLines)
+	maxLen := leftLen
+	if rightLen > maxLen {
+		maxLen = rightLen
+	}
+
+	gap := strings.Repeat(" ", len(valStr))
+	for i := 0; i < maxLen; i++ {
+		left := strings.Repeat(" ", leftWidth)
+		if i < leftLen {
+			left = leftLines[i]
+		}
+		right := strings.Repeat(" ", rightWidth)
+		if i < rightLen {
+			right = rightLines[i]
+		}
+		result = append(result, left+gap+right)
+	}
+
+	return result
+}
+
 func PrintTree(root *TreeNode) {
 	if root == nil {
 		fmt.Println("[]")
