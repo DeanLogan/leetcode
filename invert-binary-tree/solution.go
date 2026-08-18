1c91c3ff3d9df052dfe77e26c47c03bc6e21dd2b
Dean Logan <deanlogan42@gmail.com>
2026-08-18 14:26:08 +0100

    Completed invert binary tree problem

--- invert-binary-tree/solution.go
+++ invert-binary-tree/solution.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	. "github.com/DeanLogan/leetcode/libs"
+)
+
 func main() {
-	// TODO: implement Invert Binary Tree
+	tree1 := BuildTree([]int{4,2,7,1,3,6,9})
+	ans1 := invertTree(tree1)
+	PrintTree(ans1)
+	PrintTreeFancy(ans1)
+
+	tree2 := BuildTree([]int{2,1,3})
+	ans2 := invertTree(tree2)
+	PrintTree(ans2)
+	PrintTreeFancy(ans2)
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root != nil {
+        root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+    }
+    return root
 }
