75c51eea17d99a8ad931ee171cbb7741d4442293
Dean Logan <deanlogan42@gmail.com>
2026-08-19 14:50:22 +0100

    updated same tree problem with new tree lib

--- same-tree/solution.go
+++ same-tree/solution.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"fmt"
+	. "github.com/DeanLogan/leetcode/libs"
 )
 
-type TreeNode struct {
-	Val int
-	Left *TreeNode
-	Right *TreeNode
-}
+func main() {
+	p1 := BuildTree([]int{1,2,3})
+	q1 := BuildTree([]int{1,2,3})
+	fmt.Println(isSameTree(p1, q1))
 
-func main(){
-	p1 := TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}
-	q1 := TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}
-	p2 := TreeNode{1, &TreeNode{2, nil, nil}, nil}
-	q2 := TreeNode{1, nil, &TreeNode{2, nil, nil}}
-	p3 := TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{1, nil, nil}}
-	q3 := TreeNode{1, &TreeNode{1, nil, nil}, &TreeNode{2, nil, nil}}
+	p2 := BuildTree([]int{1,2})
+	q2 := BuildTree([]int{1,-1, 2})
+	fmt.Println(isSameTree(p2, q2))
 
-	fmt.Println(isSameTree(&p1, &q1))
-	fmt.Println(isSameTree(&p2, &q2))
-	fmt.Println(isSameTree(&p3, &q3))
+	p3 := BuildTree([]int{1,2,1})
+	q3 := BuildTree([]int{1,1,2})
+	fmt.Println(isSameTree(p3, q3))
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
